c2e63dd3a136194b3df42b01143e6375f1548f82
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-09 12:23:10 +0300

    init: start RSS Minesweeper task

---
+++ minesweeper/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const config = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+    assetModuleFilename: "assets/[name][ext]",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new ESLintPlugin({
+      fix: true
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+
+      {
+        test: /\.(jpe?g|png|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    config.devtool = "source-map";
+    config.target = "web";
+  } else {
+    config.devtool = false;
+    config.target = "browserslist";
+  }
+  return config;
+};
