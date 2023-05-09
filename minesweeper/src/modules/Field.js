bd3099d4117685286969983c5bbbd4f6f888267b
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-09 19:49:35 +0300

    feat: add field module

---
+++ minesweeper/src/modules/Field.js
@@ -0,0 +1,37 @@
+export default class Field {
+  constructor(difficulty = 'medium') {
+    this.difficulty = difficulty;
+    this.size = this.getSizeFromDifficulty();
+  }
+
+  getSizeFromDifficulty() {
+    switch (this.difficulty) {
+      case 'easy':
+        return 10;
+      case 'medium':
+        return 16;
+      case 'hard':
+        return 20;
+      default:
+        return 16;
+    }
+  }
+
+  renderField() {
+    const field = document.createElement('div');
+    field.className = `field field_${this.difficulty}`;
+    field.id = 'field';
+    for (let i = 0; i < this.size * this.size; i += 1) {
+      const cell = Field.renderCell();
+      field.append(cell);
+    }
+    return field;
+  }
+
+  static renderCell() {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    cell.setAttribute('status', 'hidden');
+    return cell;
+  }
+}
