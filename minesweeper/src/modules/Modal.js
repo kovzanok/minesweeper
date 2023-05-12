72236e71bfdeeac4e003951829b3abafc73c8b11
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-12 09:05:29 +0300

    feat: add ability to remove modal

--- minesweeper/src/modules/Modal.js
+++ minesweeper/src/modules/Modal.js
@@ -1,13 +1,25 @@
 export default class Modal {
   static renderModal() {
     const modal = document.createElement('div');
-    modal.className = 'modal';
 
+    modal.addEventListener('click', Modal.removeModal);
+    modal.className = 'modal';
+    window.dispatchEvent(new Event('timerPause'));
     const modalBody = document.createElement('div');
     modalBody.className = 'modal__body';
 
     modal.append(modalBody);
 
     return modal;
+  }
+
+  static removeModal(e) {
+    if (
+      !e.target.classList.contains('modal__body')
+      && !e.target.closest('.modal__body')
+    ) {
+      window.dispatchEvent(new Event('timerGo'));
+      e.target.remove();
+    }
   }
 }
