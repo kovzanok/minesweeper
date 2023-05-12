3a79665f9855bc80d73329ec980e4975ad2b7474
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-12 07:53:08 +0300

    feat: add app module as entry point to application

---
+++ minesweeper/src/modules/App.js
@@ -0,0 +1,32 @@
+import Game from './Game';
+
+export default class App {
+  constructor() {
+    this.mines = App.getMinesCount() || 10;
+    this.difficulty = App.getDifficulty() || 'medium';
+    this.savedGame = App.getGameFromLocalStorage();
+  }
+
+  start() {
+    const game = new Game(this.mines, this.difficulty, this.savedGame);
+    game.start();
+  }
+
+  static getGameFromLocalStorage() {
+    const savedGame = window.localStorage.getItem('saveGameArr');
+    if (savedGame) {
+      return JSON.parse(savedGame);
+    }
+    return {};
+  }
+
+  static getDifficulty() {
+    const difficulty = window.localStorage.getItem('difficultyValue');
+    return difficulty;
+  }
+
+  static getMinesCount() {
+    const mines = window.localStorage.getItem('minesCountValue');
+    return mines;
+  }
+}
